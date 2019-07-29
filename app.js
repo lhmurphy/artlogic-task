a1b77179166f62e166d0a8cd621cc891e0d0e54d
Laura Murphy <laurahelenamurphy@gmail.com>
2019-07-29 21:28:35 +0100

    Dropdown jQuery working

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 $(document).ready(() => {
   console.log('this is working')
 
+  $(document).ready(function() {
+    $('.faq dd').hide()
+    $('.faq dt').click(function(){
+      $(this).next().slideToggle('slow')
+    })
+  })
 })
