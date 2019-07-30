1e344f79e28009a9675d705a587e3adc1717d0cb
Laura Murphy <laurahelenamurphy@gmail.com>
2019-07-30 12:57:45 +0100

    Added slideup to FAQs and started responsive design

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ $(document).ready(() => {
   $(document).ready(function() {
     $('.faq dd').hide()
     $('.faq dt').click(function(){
+      $('.faq dd').slideUp()
       $(this).next().slideToggle('slow')
       $(this).find('.fas').toggleClass('fa-sort-up')
     })
